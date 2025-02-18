1d6771ff0737cafee2056baeb0ffbc48a9edfe4a
Adrian Flores <aj501214@gmail.com>
2025-02-18 18:57:18 +0800

    Abou Game and Devs

---
+++ abt_devs.php
@@ -0,0 +1,54 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="styles/admin_styles.css">
+	<title>About the Developers</title>
+</head>
+   <style>
+        #heading {
+            text-align: center;
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+        .main_container {
+            width: 400px;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: #fff;
+        }
+        .content {
+            font-size: 14px;
+            line-height: 1.6;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid main_container">
+        <h4 id="heading">About MindYourWords</h4>
+
+        <div class="content">
+            <p>MindYourWords is a web-based game platform designed to help learners improve their English vocabulary and spelling skills through fun and engaging gamified learning experiences. It features an interactive user interface, providing enjoyable and effective ways to learn.</p>
+            <p>Our mission is to address the common spelling mistakes learners encounter due to the complex structure of the English language that often leads to confusion and errors. We developed a web-based game platform to offer learners an interactive and educational environment to further develop their English vocabulary and spelling skills, helping them to boost their confidence, expand their knowledge, and improve their English language proficiency.</p>
+        </div>
+
+        <h4 id="heading">About the Developers</h4>
+        <div class="content">
+        <p>We, the developers of MindYourWords—Josh Andre Dela Cruz, Adrian Flores, Aaron Hernandez, Icermi Ribano, and Vince Taule—are 2nd Year College students from National University Fairview. As our final project for Database Management 2 and Website Programming, We are committed to sharing our knowledge and expertise by creating an educational web-based platform that offers English-related content, combining a user-friendly and interactive experience for learners for an effective and enjoyable way of learning.</p>
+        <p>We believe that learning should be fun and engaging yet effective. With the latest advancement of modern technology, our team integrates it in gamification with educational materials. Our goal is to provide an accessible and engaging educational platform that can help learners to further develop and enhance their English vocabulary and spelling skills in a way that is both meaningful and entertaining.</p>
+        </div>
+
+        <input type="button" class="btn w-30 btn-primary mt-2 p-2" value="Back" onclick="goBack()">
+	    <script>
+	        function goBack() {
+	            window.history.back();
+	        }
+	    </script>
+    </div>
+</body>
+
