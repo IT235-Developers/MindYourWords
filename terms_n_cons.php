671949789c81c29aae352c6ec26ccbe2c6232819
Adrian Flores <aj501214@gmail.com>
2025-02-18 18:17:21 +0800

    Terms and Cons Page

---
+++ terms_n_cons.php
@@ -0,0 +1,104 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="styles/admin_styles.css">
+	<title>Terms and Cons</title>
+</head>
+  <style>
+        #heading {
+            text-align: center;
+        }
+        #greetings {
+            font-size: 15px;
+        }
+        .main_container {
+            width: 400px;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: #fff;
+        }
+        .content {
+            font-size: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid main_container">
+        <h4 id="heading">Terms and Conditions</h4>
+
+        <div>
+            <p id="greetings">Welcome to MindYourWords! By using this web-based app, you agree to the following terms and conditions. Please read these terms carefully before proceeding.</p>
+        </div>
+
+        <div class="content">
+            <h6>Purpose</h6>
+            <ul>
+                <li>Use the platform in accordance with these terms and applicable rules and regulations.</li>
+                <li>Participate in tests and challenges without cheating.</li>
+                <li>Users agree to use the game in a lawful manner and not to upload, share, or promote any content that is offensive, harmful, or illegal.</li>
+            </ul>
+
+            <h6>User Responsibilities</h6>
+            <ul>
+                <li>Use the platform in accordance with these terms and applicable rules and regulations.</li>
+                <li>Participate in tests and challenges without cheating.</li>
+                <li>Users agree to use the game in a lawful manner and not to upload, share, or promote any content that is offensive, harmful, or illegal.</li>
+            </ul>
+
+            <h6>User Content</h6>
+            <ul>
+                <li>Users can submit and share content on the platform, granting MindYourWords a non-exclusive, worldwide, royalty-free license to use and display it. Users are responsible for ensuring their content doesn’t violate any rights or laws.</li>
+            </ul>
+
+            <h6>Limitations and Liability</h6>
+            <ul>
+                <li>MindYourWords is not liable for any direct or indirect damages, losses, or harm resulting from the use of the game.</li>
+                <li>Play the game at your own discretion.</li>
+            </ul>
+
+            <h6>Prohibited Activities</h6>
+            <ul>
+                <li>Avoid typing unnecessary, rude and malicious types of words.</li>
+                <li>Uploading or sharing content that is harmful, offensive, or illegal.</li>
+            </ul>
+
+            <h6>Privacy and Data Security</h6>
+            <ul>
+                <li>User’s information will be handled and protected according to our Privacy Policy.</li>
+                <li>While we apply security measures, we cannot ensure total data security.</li>
+            </ul>
+
+            <h6>Termination</h6>
+            <ul>
+                <li>MindYourWords reserves the right to suspend or terminate your access to the platform at any time for any violation of its terms and conditions.</li>
+            </ul>
+
+            <h6>Changes to Terms</h6>
+            <ul>
+                <li>These terms and conditions may update at any time.</li>
+                <li>By continuing to use this platform, you acknowledge and accept our updated terms.</li>
+            </ul>
+
+            <h6>Governing Law</h6>
+            <ul>
+                <li>These terms are governed by and construed in accordance with the laws of the Philippines.</li>
+                <li>Any disagreements related to these terms will be settled under these laws.</li>
+            </ul>
+
+            <h6>Contact Information</h6>
+            <ul>
+                <li>If you have any questions or concerns, please contact us at:</li>
+                <li>Email: <a href="">mindyourwords@email.com</a></li>
+            </ul>
+        </div>
+
+       <input type="button" id="btn_accept" class="btn w-100 btn-primary mt-2 p-2" value="Accept" onclick="location.href='user_homepage.php'">
+</body>
+</html>
+
