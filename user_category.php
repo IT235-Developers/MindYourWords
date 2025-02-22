a79da60df0c1587ff8a36d08bdcd820f5fe78837
Icermi Ribano <icermiribano@gmail.com>
2025-02-23 04:25:34 +0800

    Remove logic for checking for questions availability

--- user_category.php
+++ user_category.php
@@ -78,19 +78,17 @@
 
                 if ($resDisplayLevel->num_rows > 0) {
                     while ($row = $resDisplayLevel->fetch_assoc()) {
-                        if (isQuestionsAvailable($con, $row)) {
                 ?>
-                            <div class="col-12 col-md-6 g-3">
-                                <form action="user_level.php" method="POST">
-                                    <input type="hidden" name="txt_categoryHID" value="<?= $row['categoryID'] ?>">
-                                    <input type="hidden" name="txt_levelHID" value="<?= $row['levelID'] ?>">
-                                    <button type="submit" class="category_level_container rounded text-center bg-white">
-                                        <p><?= $row['levelName'] ?></p>
-                                    </button>
-                                </form>
-                            </div>
+                        <div class="col-12 col-md-6 g-3">
+                            <form action="user_level.php" method="POST">
+                                <input type="hidden" name="txt_categoryHID" value="<?= $row['categoryID'] ?>">
+                                <input type="hidden" name="txt_levelHID" value="<?= $row['levelID'] ?>">
+                                <button type="submit" class="category_level_container rounded text-center bg-white">
+                                    <p><?= $row['levelName'] ?></p>
+                                </button>
+                            </form>
+                        </div>
                 <?php
-                        }
                     }
                 }
                 ?>
