5cd6cc2c941c205ae1e08fda2b8ddc421d30580b
Icermi Ribano <icermiribano@gmail.com>
2025-02-24 06:49:09 +0800

    Output user_stats components

--- user_statistics.php
+++ user_statistics.php
@@ -45,28 +45,28 @@
         <div class="row">
             <img src="images/myw-secondary-logo.svg" class="secondary_logo">
 
-            <h1 class="fs-2 text-center">Hello, Username!</h1>
+            <h1 class="fs-2 text-center">Hello, <?= $username ?></h1>
 
             <!-- Outer flex container with wrapping -->
             <div class="d-flex flex-wrap outer-container mt-4">
                 <!-- average score -->
                 <div class="d-flex flex-column cell">
-                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center">9</div>
+                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center"><?= $averageScore ?></div>
                     <div class="p-2 custom-text-color fs-3 text-center">Average Score</div>
                 </div>
                 <!-- total games played -->
                 <div class="d-flex flex-column cell">
-                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center">100</div>
+                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center"><?= $totalGamesPlayed ?></div>
                     <div class="p-2 custom-text-color fs-3 text-center">Total Games Played</div>
                 </div>
                 <!-- highest score -->
                 <div class="d-flex flex-column cell">
-                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center">18</div>
+                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center"><?= $highestScore ?></div>
                     <div class="p-2 custom-text-color fs-3 text-center">Highest Score</div>
                 </div>
                 <!-- winning rate -->
                 <div class="d-flex flex-column cell">
-                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center">83%</div>
+                    <div class="p-2 custom-text-color fs-3 fw-bolder text-center"><?= $winningRate ?>%</div>
                     <div class="p-2 custom-text-color fs-3 text-center">Winning Rate</div>
                 </div>
             </div>
