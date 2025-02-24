d4f3585bc5b9e6a484596d66e92ee8e7982b7403
Scoofszlo <scoofszlo@gmail.com>
2025-02-24 17:22:24 +0800

    Fix missing background image at `user_homepage.php`

--- user_homepage.php
+++ user_homepage.php
@@ -32,7 +32,7 @@ function isLevelsAvailable($con, $row) {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="styles/admin_styles.css">
     </head>
-    <body>
+    <body class="bg-img">
         <div class="modal" id="logout_modal" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content" style="border-radius: 10px;">
