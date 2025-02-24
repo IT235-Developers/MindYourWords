ec539560300fe09c0f93c607528793f5f9f35f14
Scoofszlo <scoofszlo@gmail.com>
2025-02-24 16:07:14 +0800

    Slightly modify the UI of modal and buttons

--- user_homepage.php
+++ user_homepage.php
@@ -33,33 +33,27 @@ function isLevelsAvailable($con, $row) {
         <link rel="stylesheet" type="text/css" href="styles/admin_styles.css">
     </head>
     <body>
-        <div class="modal " id="logout_modal" tabindex="-1">
+        <div class="modal" id="logout_modal" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Logout</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Are you sure you want to logout?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <form action="auth/logout.php" method="POST">
-                        <button type="submit" class="btn btn-danger">Logout</button>
-                    </form>
-                </div>
+                <div class="modal-content" style="border-radius: 10px;">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Logout</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to logout?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <form action="auth/logout.php" method="POST">
+                            <button type="submit" class="btn btn-danger">Logout</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="container-fluid main_container" style="padding-top: 30px;">
             <div class="row mb-3">
-                <div>
-                    <button type="submit" class="icon_button btn-danger mt-3 float-end" data-bs-toggle="modal" data-bs-target="#logout_modal"><img src="resources/logout_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="Logout icon"></button>
-                    <form action="user_statistics.php" method="POST">
-                        <button type="submit" class="icon_button btn-success me-2 mt-3 float-end"><img src="resources/monitoring_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt=""></button>
-                    </form>
-                </div>
                 <img src="images/myw-secondary-logo.svg" class="secondary_logo">
                 <h3 class="welcome_header text-center mb-4">
                     <?php 
@@ -92,6 +86,12 @@ function isLevelsAvailable($con, $row) {
                             }
                         }
                     ?>
+                </div>
+                <div class="d-flex flex-row mt-3 justify-content-end gap-1">
+                    <form action="user_statistics.php" method="POST">
+                        <button type="submit" class="icon_button me-2 mt-3" style="background-color: #46A33A"><img src="resources/monitoring_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt=""></button>
+                    </form>
+                    <button type="submit" class="icon_button mt-3" data-bs-toggle="modal" style="background-color: #C33131" data-bs-target="#logout_modal"><img src="resources/logout_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="Logout icon"></button>
                 </div>
             </div>
 
