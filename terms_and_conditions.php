bf6fdd6d3cc4de76e534fc12013cf382e14b24ac
Icermi Ribano <icermiribano@gmail.com>
2025-02-27 11:21:12 +0800

    Update favicon

--- terms_and_conditions.php
+++ terms_and_conditions.php
@@ -5,7 +5,7 @@
 	   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="styles/terms_and_conditions.css">
-    <link rel="icon" type="image/svg+xml" href="images/myw-primary-logo.svg">
+    <link rel="icon" type="image/svg+xml" href="images/myw_favicon.svg">
 	<title>Terms and Cons</title>
 </head>
 </head>
